f3f67dca2c3b1c8389ee62d6858dab9ddb9faba8
Ngô Hải Đăng <dangpro362@gmail.com>
2026-04-03 15:41:37 +0700

    Assignment 2.2.4

--- Lab01/HelloNameDialog.java
+++ Lab01/HelloNameDialog.java
@@ -4,6 +4,7 @@ public class HelloNameDialog{
         String result;
         result = JOptionPane.showInputDialog("Please enter your name: ");
         JOptionPane.showMessageDialog(null, "Hi "+ result + "!");
+        
         System.exit(0);
     }
 }
